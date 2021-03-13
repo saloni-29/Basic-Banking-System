756cc3ec675c9c7520b9f5df5bf286ca1a21a580
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-14 02:13:17 +0530

    Add files via upload

--- viewcustomers.php
+++ viewcustomers.php
@@ -1,132 +1,48 @@
-<html>
-<head>
-<title></title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>TSF BANK</title>
+    <link rel="stylesheet" href="css/style2.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
 
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    
+    </head>
+    
+    <body>
+        
+        <!-- Nav bar -->
+        <nav class="navbar py-2 navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand font-weight-bold text-white text-center"><h2>TSF BANK</h2></a>
+                <button class="navbar-toggler text-white " type="button" data-toggle="collapse" data-target="#collapsenavbar">
+                    <span class="navbar-toggler-icon" style="background:white;"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-8 mb-lg-0">
+                        <li class="nav-item py-3">
+                            <a href="index.php" class="nav-link text-white"><strong>About</strong></a></li>
+                        <li class="nav-item dropdown py-3">
+                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold;"><strong>Actions</strong></a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item py-3" href="money.php">Money Transfer</a></li>
+                                <li><a class="dropdown-item py-3" href="history.php">Transaction History</a></li>
+                                <li><a class="dropdown-item py-3" href="viewcustomers.php">View Customers</a></li>
+                                </li></ul>
+                        <li class="nav-item py-3">
+                            <a class="nav-link text-white" href="contact.php"style="font-weight: bold;"><strong>Contact Us</strong></a>
+                        </li>
+                    </ul>
+                 </div>
+            </div>
+        </nav>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;400&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-*{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-}
-
-.navbar
-{
-    width:100%;
-    white-space:nowrap;
-    background-color: #5E11A3;
-}
-.main_div
-{
-    width:100%;
-    height:100vh;
- 
-}
-*{
-    margin:0;
-    padding:0;
-   box-sizing:border-box;
-}
-.display_table
-{
-    width:100vw;
-    height:100vh;
-    display:flex;
-    flex-direction:column;
-    justify-content: center;
-    text-align:center;
-}
-.center_div
-{
-    width:90vw;
-    height:80vh;
-    background-image:url('images/2.jpg');
-    background-repeat:no-repeat;
-    background-size:100%;
-    padding:20px 0 0 0;
-    box-shadow:0 10px 20px 0 rgba(0,0,0,0.03);
-    border-radius:10px;
-    margin-left:30px;
-}
-h1
-{
-    font-size:18px;
-    color:#000;
-    text-align:center;
-    margin-top:-20px;
-    margin-bottom:20px;
-}
-table
-{
-    border-collapse:collapse;
-    background-color:black;
-    box-shadow:0 10px 20px 0 rgba(0,0,0,0.03);
-    border-radius: 10px;
-    border-collapse:collapse;
-    table-layout:fixed;
-    opacity:0.7;
-    color:#F7CC1A;
-    font-weight:bold;
-    margin:auto;
-}
-th,td
-{
-  border:1px solid #f2f2f2;
-   padding:8px 30px;
-  text-align:center;
-  opacity:0.9;
-  color:#04FB73 ; 
-}
-th{
-    text-transform:uppercase;
-    font-weight:500;
-}
-td
-{
-    font-size:13px;
-}
-
-</style>
-</head>
-<body>
-<div class="main_div">
- 
-     <div class="navbar navbar-expand-md">
-   
-      <a href="#" class="navbar-brand font-weight-bold text-white text-center">TSF BANK</a>
-      <button class="navbar-toggler text-white " type="button" data-toggle="collapse" data-target="#collapsenavbar">
-      <span class="navbar-toggler-icon" style="background:white;"></span>
-      </button>
-     
-       <div class="collapse navbar-collapse text-center" id="collapsenavbar">
-          <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                  <a href="index.php" class="nav-link text-white">ABOUT</a></li>
-              <li class="nav-item">
-                  <a href="viewcustomers.php" class="nav-link text-white">VIEW CUSTOMER</a></li>
-              <li class="nav-item">
-                  <a href="viewcustomers.php" class="nav-link text-white"></a></li>
-              <li class="nav-item">
-                  <a href="#" class="nav-link text-white">CONTACT</a></li>    
-               </ul>
-        </div>
-     </div>
-
-      
-           
           <div class="display_table">
                  <h1>Customer Details</h1>
                  <div class="center_div">
