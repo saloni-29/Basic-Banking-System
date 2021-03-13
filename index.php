eedbf65fcddd26babfccf2c6d18e7ae612cfd8c2
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-13 23:42:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <title>TSF BANK</title>
     <link rel="stylesheet" href="css/style1.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
 </head>
 
 <body>
