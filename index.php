1f26f4be788bf44133de521dfbfb5875b8915e02
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-14 16:16:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,7 @@
            <footer class="text-center text-lg-start foot-dark bg-dark">
                 
                 <!-- Copyright -->
-                <div class="text-center p-3  style="background-color: #5E11A3">
+                <div class="text-center p-3  style= background-color: #5E11A3">
                     © 2021 Copyright - Made with 🧡 by Saloni Raorane :
                     <a class="text-dark" href="https://www.thesparksfoundationsingapore.org/" target="_blank"> The Sparks Foundation <img src="images/tsf.png" alt="" width="40" height="35"></a>
                 </div>
