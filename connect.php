8c5626c673df407b625f9fd9c8bd2e0117056863
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-14 17:17:12 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $username = "id15696174_customer";
 $password="";
 $server ="localhost";
-$db = "id15696174_customerdetails";
+$db = "bankuser.sql";
 
 
 $con = mysqli_connect($server,$username,$password,$db);
