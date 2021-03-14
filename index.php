d561e1e93d24a151638c69607fdae8eb49d8ff06
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-15 00:31:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -68,8 +68,16 @@
                 <div class="carousel-item">
                     <img src="background/bg3.jpg" width="100" height="500" class="d-block w-100" alt="...">
                 </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </a>
             </div>
-        </div>
+    </div>
   
         <!-- footer -->
         <div class="foot">
