a1e0b26c68f76e042b561017b2375fdcab15fe93
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-15 00:07:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,36 +47,36 @@
                 <img src="images/logo1.webp" alt="Bank" width="75" height="75" style="font-weight: bold;"> <strong>Welcome To TSF Bank !</strong> <img src="images/trust.webp" alt="Bank" width="75" height="75">
             </h2>
         </class>
-        
-        <!-- Carousel -->
-        <div id="carouselSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval=500>
-            <div class="carousel-inner">
-                <ol class="carousel-indicators">
-                    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
-                    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
-                    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
-                </ol>
-                <div class="carousel-item active">
-                    <img src="images/bg1.jpg" width="100" height="500" class="d-block w-100" alt="..." >
-                </div>
-                <div class="carousel-item">
-                    <img src="images/bg2.jfif" width="100" height="500" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/bg3.jfif" width="100" height="500" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="images/bg3.jpg" width="100" height="500" class="d-block w-100" alt="...">
-                </div>
+    
+    <!-- Carousel -->
+    <div id="carouselExampleCaptions" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="background/bg1.jpg" width="1000" height="375" class="d-block w-100" alt="...">
+
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </a>
+            <div class="carousel-item">
+                <img src="background/bg2.jfif" width="1000" height="375" class="d-block w-100" alt="...">
+
+            </div>
+            <div class="carousel-item">
+                <img src="background/bg3.jfif" width="1000" height="375" class="d-block w-100" alt="...">
+
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </a>
     </div>
   
         <!-- footer -->
