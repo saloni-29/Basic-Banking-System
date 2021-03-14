b4f1a384708b41011beebdb544ee9918b326da5b
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-15 01:53:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                     <span class="navbar-toggler-icon" style="background:white;"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsenavbar">
-                    <ul class="navbar-nav mb-8 mb-lg-0 ml-auto">
+                    <ul class="navbar-nav ml-auto mb-8 mb-lg-0">
                         <li class="nav-item py-3">
                             <a href="index.php" class="nav-link text-white"><strong>About</strong></a></li>
                         <li class="nav-item dropdown py-3">
