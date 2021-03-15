1646da16f0ce1a4ae52fd819ea570a44dd54aa6f
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-16 01:00:25 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,9 +5,10 @@ echo "Welcome to the stage where we are ready to get connected to a database <br
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "bankuser";
 
 // Create a connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password,$dbname);
 
 // Die if connection was not successful
 if (!$conn){
