f2a421b7127326e29dba44a7b8cef8914749e25e
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-16 11:47:43 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -8,4 +8,19 @@ $dbname = "bankuser";
 // Create a connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
 
+
+if($conn)
+//echo "Connection successful";
+{
+    <script>
+        alert('Connection Successful');
+    </script>
+    <?php
+}
+else
+{
+    echo "No connection";
+    die("no connection".mysqli_connect_error());//exit
+}
+
 ?>
