29e7fbf01520118f6387d372b68a0ab788576921
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-16 13:52:53 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -11,11 +11,7 @@
     <link rel="stylesheet" href="css/style1.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
-    <style>
-        .navbar-nav {
-            margin-left: auto;
-        } 
-    </style> 
+
     
     </head>
     
@@ -28,16 +24,16 @@
                 <button class="navbar-toggler text-white " type="button" data-toggle="collapse" data-target="#collapsenavbar">
                     <span class="navbar-toggler-icon" style="background:white;"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="collapsenavbar">
-                    <ul class="navbar-nav mb-8 mb-lg-0">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-8 mb-lg-0">
                         <li class="nav-item py-3">
                             <a href="index.php" class="nav-link text-white"><strong>About</strong></a></li>
-                        <li class="nav-item dropdown py-3">
-                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="font-weight: bold;"><strong>Transactions</strong></a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item py-3" href="money.php">Money Transfer</a></li>
-                                <li><a class="dropdown-item py-3" href="history.php">Transaction History</a></li>
-                                </li></ul>
+                        <li class="nav-item py-3">
+                            <a class="nav-link text-white" href="money.php"style="font-weight: bold;"><strong>Money Transfer</strong></a>
+                        </li>
+                        <li class="nav-item py-3">
+                            <a class="nav-link text-white" href="histoy.php"style="font-weight: bold;"><strong>Transfer History</strong></a>
+                        </li>
                         <li class="nav-item py-3">
                             <a class="nav-link text-white" href="contact.php"style="font-weight: bold;"><strong>Contact Us</strong></a>
                         </li>
