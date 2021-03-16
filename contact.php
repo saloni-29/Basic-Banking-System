854fe73a23d93c5b1b2a0c7188556e05b7514cbc
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-16 14:16:36 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -41,9 +41,7 @@
         </nav>
 
     <class class="welcome">
-        <h2>
-            <img src="images/friend.webp" alt="Bank" width="75" height="75" style="font-weight: bold;"> <strong>We are here to help you !</strong> <img
-                src="images/trust.webp" alt="Bank" width="75" height="75">
+        <a href="#" class="navbar-brand font-weight-bold text-white text-center"><h2> <strong>We are here to help you !</strong> <img src="images/trust.webp" alt="Bank" width="75" height="75">
         </h2>
     </class>
  
