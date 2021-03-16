949911d32820010154f9a0d0299b343243bd1c12
Saloni Raorane <68276358+saloni-29@users.noreply.github.com>
2021-03-16 19:00:51 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -45,7 +45,7 @@
             <a href="#" class="navbar-brand font-weight-bold text-center"><h2><strong>We are here to help you !</strong><img src="images/trust.webp" alt="Bank" width="75" height="75"></h2>
         </class>
             
-            <div class="col-4 mx-auto text-center p-3 mb-2">
+            <div class="col-6 mx-auto p-3 mb-2">
                 <img src="images/12.png" class="img-fluid" alt= " " width="200" height="200" >
             </class>
             
